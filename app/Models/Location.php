fcf77426b385e2221b42bd72c0ca6c1a80499031
nethukw <nethukzz@gmail.com>
2026-09-17 15:37:29 +0530

    Fixed Location.php file

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
+    
+    protected $table = 'Locations';
+
     protected $fillable = ['vehicle_id', 'latitude', 'longitude', 'speed', 'ignition_on', 'heading', 'recorded_at'];
 
     // The $casts property is used to convert attributes to a specific data type when they are accessed or set. In this case, it ensures that the ignition_on attribute is treated as a boolean, and speed and heading are treated as floats.
